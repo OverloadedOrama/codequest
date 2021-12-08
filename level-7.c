ebf05f9144cb5a7e0ede70ec4666b8e64e877561
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-12-08 20:14:38 +0200

    init

---
+++ level-7.c
@@ -0,0 +1,23 @@
+ 
+  /* auto_format=off ;) */#include <stdio.h>
+        int forty_two=42;int magic(int foo){const int magic_number=1337;
+    return foo-magic_number;}int main(){const char oops[]="Unfortunately, 'strings' does"
+" not seem to help :)"; forty_two                               +=5;printf("%c",forty_two);
+forty_two-=2;/*(^-^) (^v^)                                      (UwU)*/char _while[]="while";
+size_t i; for(i=0;i<sizeof                                      (_while)/ sizeof(_while[0])-1;
+i++){if(i==2) printf ("%c",
+oops[8]);else printf(
+"%c", _while[i]);}int x=
+1452;int magic_array[]
+={0,-3,-1,5,0,-11,5,
+-7};int j;for(j=0;j<8;
+++j){x+=magic_array[j]
+;printf("%c",magic(x)
+);}char couch[]="sofa";
+int o=1;printf("%c%c"
+,forty_two,couch[o++]);printf(                                  "%c%c",couch[o++],forty_two);
+ printf("%c%c",couch[o++],forty_two                     );int orx[]={32,171,223,139,216
+    ,83,144,222,97};int ory[]={68,194,185,237,189,33,245,176,21};int k;for(k=0;k<9;++k)
+         printf("%c",orx[k]^ory[k]);printf("%c", forty_two);int type=1684957547;
+                 int l;for(l=0;l<4;++l){printf("%c",type&255);type=type
+                                >>8;}printf("\n");return 0;}
